4a640b64855e8e8cccda6b6603200f891732dd67
Anu Tomy <anuatomy@gmail.com>
2018-06-14 20:35:00 -0400

    create a helper class StudentDataUtil

---
+++ servletdemo/src/com/jsptutorials/servletdemo/mvc/StudentDataUtil.java
@@ -0,0 +1,24 @@
+package com.jsptutorials.servletdemo.mvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDataUtil {
+	
+	public static List<Student> getStudents(){
+		
+		//Create an empty list
+		List<Student> students = new ArrayList<>();
+		
+		//Add sample data
+		students.add(new Student("Mary", "Ann", "mary@gmail.com"));
+		students.add(new Student("Jim", "John", "Jim@gmail.com"));
+		students.add(new Student("Tom", "Alex", "tom@gmail.com"));
+		
+		//Return the list
+		return students;	
+		
+	}
+		
+
+}
